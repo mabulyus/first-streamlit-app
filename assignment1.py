be2895536c5248fe1b65410c9f2148a3eb2c3456
mabulyus <93688188+mabulyus@users.noreply.github.com>
2021-11-11 19:14:11 -0800

    Create assignment1.py

---
+++ assignment1.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import streamlit as st
+import pandas as pd
+
+import plotly.express as px
+
+st.title("MABA 6490")
+st.header("Assignment 1:Product Idea and Reflection")
+
+
+st.subheader("♠♣   Mohamed Abulyusr   ♥♦")
+
+st.markdown(" ")
+st.markdown(" ")
+st.markdown(" ")
+
+st.markdown("## 🎲 The Application")
+st.markdown(" ")
+
+st.markdown("##### **♟ User Input ♟**")
+
+st.markdown("* User will input historic price data of a particular stock and the algorithm will compute a future price"
+            " prediction of the particular stock.")
+st.markdown("* In addition, the user will be able to determine how far in the future they would like to see the "
+            "prediction up to a maximum of 2 years. This will possibly be achieved through a slider option or "
+            "user text input.")
+st.markdown(" ")
+
+st.markdown("##### **♟ Modeling ♟**")
+
+st.markdown("* A time series forecasting modeling technique will be used. An ARIMA modeling approach is being"
+            " considered alongside other time series forecasting models.")
+st.markdown(" ")
+
+st.markdown("##### **♟ Output ♟**")
+st.markdown("* The output will be a graph showing how the stock price is anticipated to trend up until the time period"
+            " that the user specifies. ")
+st.markdown(" ")
+st.markdown(" ")
+
+st.markdown("## 🎲 Reflection")
+st.markdown(" ")
+
+st.markdown("* Initially, I had problems understanding what we were trying to do. I was confused because everybody"
+            " was using different applications to do the same things and it just didn't make sense to me. After class,"
+            " I started to explore the programs a little bit more to try to put all the pieces together. I had some"
+            " success, but I was still a little lost.")
+st.markdown(" ")
+
+st.markdown("* Santoshi reached out to me herself and told me that she was willing to help. We met on the weekend and "
+            "she literally sat with me for 2 hours and explained everything crystal clear. I was able to do this"
+            " assignment all by myself because of her. She really helped a ton and I think she deserves the maximum"
+            " extra credit points because she really went out of her way to help. Santoshi is the real MVP.")
+
+
+
+
+
